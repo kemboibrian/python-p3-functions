fa0381dc49e1447605d0abeaaea4ea339f69e594
Brian Kemboi <brnkemboi@gmail.com>
2024-05-23 09:17:35 +0300

    complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print ("Hello, programmer!" )
     pass
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
+
+
     pass
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
-def halve(number):
-    pass
+def halve(num):
+    return num / 2
+
